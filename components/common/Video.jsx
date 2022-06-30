aff98c1da982902de099eddd387c32396808b38f
bgoette <bryantgoette@hotmail.com>
2022-06-30 10:58:18 -0500

    add jj-about-video set to autoplay and mute

---
+++ components/common/Video.jsx
@@ -0,0 +1,19 @@
+import { Player, BigPlayButton } from "video-react";
+import "node_modules/video-react/dist/video-react.css";
+
+const Video = (video) => {
+  return (
+    <div className="video">
+      <Player
+        preload="auto"
+        muted
+        autoPlay="true"
+        src={video.src}
+      >
+        <BigPlayButton position="center" />
+      </Player>
+    </div>
+  );
+};
+
+export default Video;
