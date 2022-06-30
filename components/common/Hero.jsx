0403a303520aa4ed9ea986538faddc3f956dec11
bgoette <bryantgoette@hotmail.com>
2022-06-30 16:02:14 -0500

    add video modal

--- components/common/Hero.jsx
+++ components/common/Hero.jsx
@@ -1,10 +1,19 @@
 // React Components
-import { Container } from "react-bootstrap"
+import { Modal, Container, Row, Col, Button } from "react-bootstrap";
+import { useState } from "react";
+import { Player, BigPlayButton } from "video-react";
+import "node_modules/video-react/dist/video-react.css";
 
 // Custom Components
 import BookNowCTA from "components/common/BookNowCTA";
 
 const Hero = (hero) => {
+  const [fullscreen, setFullscreen] = useState(true);
+  const [show, setShow] = useState(false);
+  function handleShow() {
+    setShow(true);
+  }
+
   let hasTitle;
   if (hero.title !== undefined) {
     hasTitle = true;
@@ -37,6 +46,57 @@ const Hero = (hero) => {
     }
   };
 
+  let hasBookNow;
+  if (hero.bookNow !== undefined) {
+    hasBookNow = true;
+  } else {
+    hasBookNow = false;
+  }
+
+  const renderBookNow = () => {
+    if (hasBookNow) {
+      return (
+        <>
+          <BookNowCTA />
+        </>
+      );
+    }
+  };
+
+  let hasVideo;
+  if (hero.video !== undefined) {
+    hasVideo = true;
+  } else {
+    hasVideo = false;
+  }
+
+  const renderVideo = () => {
+    if (hasVideo) {
+      return (
+        <>
+          <Button className="mt-4" onClick={() => handleShow()}>{hero.video.ctaLabel}</Button>
+          <Modal
+            className="modal-video"
+            show={show}
+            fullscreen={fullscreen}
+            onHide={() => setShow(false)}
+          >
+            <Modal.Header closeButton closeVariant="white"></Modal.Header>
+            <Modal.Body className="video">
+              <Player
+                preload="auto"
+                poster={hero.video.poster}
+                src={hero.video.src}
+              >
+                <BigPlayButton position="center" />
+              </Player>
+            </Modal.Body>
+          </Modal>
+        </>
+      );
+    }
+  };
+
   return (
     <section className={hero.class}>
       <Container className="hero-grid">
@@ -44,7 +104,8 @@ const Hero = (hero) => {
           <div className="hero-content">
             {renderTitle()}
             {renderText()}
-            <BookNowCTA />
+            {renderBookNow()}
+            {renderVideo()}
           </div>
         </div>
       </Container>
--- components/common/Video.jsx
+++ components/common/Video.jsx
@@ -1,21 +1,29 @@
-import { Player, BigPlayButton } from "video-react";
 import { Modal, Button, Container, Row, Col } from "react-bootstrap";
 import { useState } from "react";
+import { Player, BigPlayButton } from "video-react";
 import "node_modules/video-react/dist/video-react.css";
 
-const Video = (video) => {
-    const [fullscreen, setFullscreen] = useState(true);
-    const [show, setShow] = useState(false);
+export default function Video(block) {
+  const [fullscreen, setFullscreen] = useState(true);
+  const [show, setShow] = useState(false);
+
+  function handleShow() {
+    setShow(true);
+  }
 
   return (
-    <div className="video">
-      <Container>
-        <Player preload="auto" playsInline src={video.src} poster={video.poster}>
+    <>
+      <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            
+        <Player playsInline poster={block.poster} src={block.src}>
           <BigPlayButton position="center" />
         </Player>
-      </Container>
-    </div>
+        </Modal.Body>
+      </Modal>
+    </>
   );
-};
-
-export default Video;
+}
