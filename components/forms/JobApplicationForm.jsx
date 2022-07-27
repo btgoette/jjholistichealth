3c67ffad525acd31b1647c41dd8ceee156387169
bgoette <bryantgoette@hotmail.com>
2022-07-27 10:18:11 -0500

    update typos in jobapp

--- components/forms/JobApplicationForm.jsx
+++ components/forms/JobApplicationForm.jsx
@@ -591,14 +591,14 @@ const JobApplicationForm = () => {
             />
           </div>
           <div>
-            <label htmlFor="Todays_Datedate">
+            <label htmlFor="Todays_Date">
               Today&apos;s Date<span className="required">*</span>
             </label>
             <input
               name="Todays_Date"
               required
               type="date"
-              id="todaysdate"
+              id="Todays_Date"
               placeholder
             />
           </div>
